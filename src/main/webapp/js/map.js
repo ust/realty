7cd972642b89eb8d51a7dea60fec206066423f17
Iaroslav <ustiner@gmail.com>
2013-03-03 22:17:53 +0200

    fixed: commented ws nature

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,26 +1,10 @@
-window.map = null;
-
-window.onload = function () {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
-	} else {
-		// let some shit out..
-	}
-}
-
-function displayLocation (position) {
-	window.map = new google.maps.Map(
-		document.getElementById("map"), 
-		{	zoom: 10,
-			center: new google.maps.LatLng(
-				position.coords.latitude, 
-				position.coords.longitude
-			),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	);
-}
-
-function displayError (error) {
-	// body...
+function initialize() {
+	var mapOptions = {
+	  center: new google.maps.LatLng(-34.397, 150.644),
+	  zoom: 8,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+	var map = new google.maps.Map(document.getElementById("map"),
+	    mapOptions);
 }
